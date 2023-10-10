de9adc2cf08d820856054a0474afa8a66b06ff7c
vijay-airsonweb <120111418+vijay-airsonweb@users.noreply.github.com>
2023-10-10 18:13:24 +0530

    Updated index.js 2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Amplify } from "aws-amplify";
-// import awsconfig from "./aws-exports";
+import awsconfig from "./aws-exports";
 import "@aws-amplify/ui-react/styles.css";
 import { ThemeProvider } from "@aws-amplify/ui-react";
 
-// Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
