f1e28d913f47b73f6e2bf3561e900413b4e715a4
vijay-airsonweb <120111418+vijay-airsonweb@users.noreply.github.com>
2023-10-10 18:36:25 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Amplify } from "aws-amplify";
+// import { Amplify } from "aws-amplify";
 // import awsconfig from "./aws-exports";
 import "@aws-amplify/ui-react/styles.css";
 import { ThemeProvider } from "@aws-amplify/ui-react";
